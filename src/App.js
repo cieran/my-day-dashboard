ac180de410aa1f75155b21f61242c038fe0fb70f
Cieran Neeson <cieranneeson@gmail.com>
2019-01-10 22:45:54 +0000

    let's go let's go let's go

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './layout/NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <NavBar />
+    );
+  }
+}
+
+export default App;
---
+++ src/layout/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <React.Fragment>
+            <nav>
+                <ul>
+                    <li>hi</li>
+                </ul>
+            </nav>
+        </React.Fragment>
+    )
+}
